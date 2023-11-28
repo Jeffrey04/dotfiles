b3b978d55d3e05b596ce2d0b1eaedc3d33ad2535
Jeffrey04 <mycyberpet@yahoo.com>
2023-11-28 14:22:22 +0800

    skeleton for python installation

---
+++ base/python/install.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import json
+import subprocess
+
+with open("config.json") as f:
+    config = json.load(f)
+
+# FIXME install python2 proxy and pipx
+# subprocess.call(["sudo", "apt", "install", "-y", "python-is-python3", "pipx"])
+
+# FIXME install pyenv
+subprocess.call(["pyenv", "global", "system", "system"])
+subprocess.call(["pyenv", "install", "-f", config["configuration"]["install_version"]])
+
+subprocess.call(["pipx", "install", "poetry"])
