2e6c6d65d74c170e6186f7e23f7649cf754d79b5
Jeffrey04 <mycyberpet@yahoo.com>
2023-11-28 16:43:15 +0800

    add vim setup

---
+++ vim/install.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import json
+import os
+import subprocess
+
+with open("config.json") as f:
+    config = json.load(f)
+
+os.makedirs(f"{os.environ['HOME']}/.config/nvim", exist_ok=True)
+os.makedirs(f"{os.environ['HOME']}/.local/share/nvim/site/pack", exist_ok=True)
+subprocess.call(
+    ["stow", "-R", f"--target={os.environ['HOME']}", "--verbose=3", "content"]
+)
+
+
+cwd = os.getcwd()
+
+os.chdir(f"{os.environ['HOME']}/.config/nvim")
+subprocess.call("pyenv local 3.11.6 3.11.6", shell=True)
+subprocess.call("poetry init --name=nvim --python=^3.11 --no-interaction", shell=True)
+subprocess.call("poetry config --local virtualenvs.in-project true", shell=True)
+subprocess.call("poetry add pynvim", shell=True)
+subprocess.call("echo v20.10.0 > .nvimrc", shell=True)
+subprocess.call("nvm use", shell=True)
+subprocess.call("corepack enable", shell=True)
+subprocess.call("yarn install", shell=True)
+subprocess.call("yarn add neovim", shell=True)
+subprocess.call("nvm exec yarn install", shell=True)
+subprocess.call("rbenv local 3.2.2", shell=True)
+subprocess.call("bundle init", shell=True)
+subprocess.call("bundle add neovim", shell=True)
+subprocess.call(
+    "bundle install && bundle binstubs --force --standalone neovim", shell=True
+)
+
+os.chdir(f"{os.environ['HOME']}/.local/share/nvim/site/pack")
+
+for plugin in config["config"]["plugins"]:
+    plugin_name = plugin.split("/")[-1]
+    plugin_path = f"{plugin_name}/start"
+    os.makedirs(plugin_path)
+    subprocess.call(["git", "clone", plugin, f"{plugin_path}/{plugin_name}"])
+
+os.chdir(cwd)
