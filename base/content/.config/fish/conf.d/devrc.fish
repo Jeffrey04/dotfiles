a1775ff96f6c41072c4ea44e8cdaf7ad9a2f4f70
Jeffrey04 <mycyberpet@yahoo.com>
2024-04-25 17:38:40 +0800

    update tools and settings

--- base/content/.config/fish/conf.d/devrc.fish
+++ base/content/.config/fish/conf.d/devrc.fish
@@ -17,6 +17,9 @@ status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
 # direnv
 direnv hook fish | source
 
+# zoxide
+zoxide init fish | source
+
 # Golang
 set -U GOROOT /home/jeffrey04/.local/share/umake/go/go-lang
 set -U GOPATH $HOME/.go
@@ -27,12 +30,16 @@ set -Ua fish_user_paths (go env GOPATH)/bin
 set -Ua fish_user_paths /home/jeffrey04/.local/share/umake/bin
 
 # pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -Ua fish_user_paths $PYENV_ROOT/bin
 pyenv init - | source
 
 # aliases
 alias cat=$HOME/.cargo/bin/bat
+alias cd=z
 alias dig=$HOME/.cargo/bin/dog
 alias http=$HOME/.cargo/bin/xh
 alias ls=$HOME/.cargo/bin/lsd
-alias nvim=$HOME/Applications/nvim.appimage
-alias vim=$HOME/Applications/nvim.appimage
+alias nvim='env APPIMAGELAUNCHER_DISABLE=1 $HOME/Applications/nvim.appimage'
+alias vim='env APPIMAGELAUNCHER_DISABLE=1 $HOME/Applications/nvim.appimage'
+alias code='env APPIMAGELAUNCHER_DISABLE=1 $HOME/.local/bin/code'
