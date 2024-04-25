ff6c2e3fd794a3ef88210881a74651f667c550c0
Jeffrey04 <mycyberpet@yahoo.com>
2024-04-26 00:20:59 +0800

    nodejs update

--- nodejs/install.py
+++ nodejs/install.py
@@ -3,9 +3,22 @@ import subprocess
 
 with open("config.json") as f:
     config = json.load(f)
+    from pprint import pprint
+
+    pprint(config)
 
 subprocess.call(
-    "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash",
+    f"curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v{config['configuration']['nvm_version']}/install.sh | bash",
     shell=True,
 )
-subprocess.call("pipx install nvshim")
+proc = subprocess.Popen(
+    [
+        "/usr/bin/bash",
+        "-i",
+        "-c",
+        f"nvm install {config['configuration']['install_version']}",
+    ],
+)
+proc.communicate()
+proc.wait()
+subprocess.call("pipx install --force nvshim", shell=True)
