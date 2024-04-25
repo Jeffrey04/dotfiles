64b1b2fb2c66e3693e477bf32dba9388074b98d3
Jeffrey04 <mycyberpet@yahoo.com>
2024-04-25 20:59:30 +0800

    initialize bash on startup for mac

--- base/install.sh
+++ base/install.sh
@@ -3,6 +3,15 @@
 if [ -f /etc/skel/.bashrc ]; then
     cp /etc/skel/.bashrc $HOME/.bashrc
 else
+    cat > $HOME/.profile <<EOL
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+EOL
     touch $HOME/.bashrc
 fi
 
