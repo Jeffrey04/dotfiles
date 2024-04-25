6d6d5679b1bffd58b736f8bf09150421f2819da5
Jeffrey04 <mycyberpet@yahoo.com>
2024-04-25 23:53:15 +0800

    install cmake for starship

--- rust/install.sh
+++ rust/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+brew install --force cmake
 cargo install --git https://github.com/ogham/dog dog
 cargo install bat xh lsd zoxide starship
