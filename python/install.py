85c6d99040ccbc78ffcf446d87fbb8ac435569a8
Jeffrey04 <mycyberpet@yahoo.com>
2024-04-25 22:06:45 +0800

    use separate shell for brew command

--- python/install.py
+++ python/install.py
@@ -10,11 +10,14 @@ if platform.system() == "Linux":
     subprocess.call(
         "sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev "
         "libbz2-dev libreadline-dev libsqlite3-dev curl "
-        "libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
+        "libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev",
+        shell=True,
     )
 
 elif platform.system() == "Darwin":
-    subprocess.call("brew install --force openssl readline sqlite3 xz zlib tcl-tk")
+    subprocess.call(
+        "brew install --force openssl readline sqlite3 xz zlib tcl-tk", shell=True
+    )
 
 subprocess.call("curl https://pyenv.run | bash", shell=True)
 
@@ -22,9 +25,9 @@ subprocess.call(["pyenv", "global", "system", "system"])
 subprocess.call(["pyenv", "install", "-f", config["configuration"]["install_version"]])
 
 if platform.system() == "Linux":
-    subprocess.call(["sudo", "apt", "install", "-y", "pipx"])
+    subprocess.call(["sudo", "apt", "install", "-y", "pipx"], shell=True)
 
 elif platform.system() == "Darwin":
-    subprocess.call(["brew", "install", "--force", "pipx"])
+    subprocess.call(["brew", "install", "--force", "pipx"], shell=True)
 
 subprocess.call(["pipx", "install", "--force", "poetry"])
