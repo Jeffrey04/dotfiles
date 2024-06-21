3b4d04fbf20e4de3a0bd2799c19542766294f4e4
Jeffrey04 <mycyberpet@yahoo.com>
2024-06-21 16:33:09 +0800

    install cmake

--- rust/install.sh
+++ rust/install.sh
@@ -4,6 +4,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 if [ "$(uname)" == "Darwin" ]; then
     brew install --force cmake
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt install -y cmake
 fi
 
 $HOME/.cargo/bin/cargo install --git https://github.com/ogham/dog dog
