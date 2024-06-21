518121df2e233fe5e9f641aee4adf5bc119ed804
Jeffrey04 <mycyberpet@yahoo.com>
2024-06-21 16:10:25 +0800

    fix rust installation

--- rust/install.sh
+++ rust/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-brew install --force cmake
-cargo install --git https://github.com/ogham/dog dog
-cargo install bat xh lsd zoxide starship
+
+if [ "$(uname)" == "Darwin" ]; then
+    brew install --force cmake
+fi
+
+$HOME/.cargo/bin/cargo install --git https://github.com/ogham/dog dog
+$HOME/.cargo/bin/cargo install bat xh lsd zoxide starship
