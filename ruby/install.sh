718a0d201e9191201e3a5e23a838f88f6186adac
Jeffrey04 <mycyberpet@yahoo.com>
2024-06-21 16:07:01 +0800

    install required dependency

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt install -y libtool
+
 # FIXME update to latest version
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
 $HOME/.rbenv/bin/rbenv install -f 3.2.2
